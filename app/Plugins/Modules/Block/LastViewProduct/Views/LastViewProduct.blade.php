311860ee3f382c7147b5bd5a25e3400a4d170a4c
guangxu0924 <guangxu924@outlook.com>
2020-03-23 05:22:43 +0800

    fixed UI

--- app/Plugins/Modules/Block/LastViewProduct/Views/LastViewProduct.blade.php
+++ app/Plugins/Modules/Block/LastViewProduct/Views/LastViewProduct.blade.php
@@ -6,7 +6,7 @@
                   @foreach ($arrProductsLastView as $product_lastView)
                     <li>
                       <div class="row">
-                        <div class="col-xs-4"><a href="{{ $product_lastView->getUrl() }}"><img src="{{ asset($product_lastView->getThumb()) }}" alt="{{ $product_lastView->name }}" /></a></div>
+                        <div class="col-xs-4 text-center"><a href="{{ $product_lastView->getUrl() }}"><img src="{{ asset($product_lastView->getThumb()) }}" alt="{{ $product_lastView->name }}" /></a></div>
                         <div class="col-xs-8"><a href="{{ $product_lastView->getUrl() }}">{{ $product_lastView->name}}</a><span class="last-view">({{ $product_lastView['timelastview'] }})</span></div>
                       </div>
                     </li>
