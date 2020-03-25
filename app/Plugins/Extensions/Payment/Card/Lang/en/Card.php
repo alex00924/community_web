684d01846b09d1b9389dd02bd7d580dffcd9779d
guangxu0924 <guangxu924@outlook.com>
2020-03-25 21:22:12 +0800

    Adding Card Extension

---
+++ app/Plugins/Extensions/Payment/Card/Lang/en/Card.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title'                     => 'Card',
+    'id'                        => 'ID',
+    'card_currency'             => 'Currency',
+    'card_order_status_success' => 'Order success',
+    'card_order_status_faild'   => 'Order faild',
+    'card_public_key'           => 'Public Key',
+    'card_secrect'              => 'Secrect Key',
+];
