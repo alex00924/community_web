8df85fcda9900a0e3c3f83ba58fa28929acbd472
guangxu0924 <guangxu924@outlook.com>
2020-03-28 02:52:54 +0800

    Added questionaire language file

---
+++ resources/lang/en/questionaire.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'admin' => [
+        'title' => 'Questionaire ',
+        'create_success' => 'Create new question success!',
+        'edit_success' => 'Edit question success!',
+        'action' => 'Action',
+        'delete' => 'Delete',
+        'edit' => 'Edit',
+        'add_new' => 'Add new',
+    ],
+];
