132bcaa4d2428d2e8e7444d744e7823d750a0178
guangxu0924 <guangxu924@outlook.com>
2020-04-19 06:20:51 +0800

    updating PO name

--- app/Plugins/Extensions/Payment/PO/Lang/en/Po.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'title'                     => 'Purchase Order',
-    'id'                        => 'ID',
-    'po_currency'               => 'Currency',
-    'po_order_status_success'   => 'Order success',
-    'po_order_status_faild'     => 'Order faild',
-];
