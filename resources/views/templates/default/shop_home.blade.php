212e274d57f363f0a02b01556ec3a7cd024ec3c8
guangxu0924 <guangxu924@outlook.com>
2020-04-25 02:07:41 +0800

    Updated home page

--- resources/views/templates/default/shop_home.blade.php
+++ resources/views/templates/default/shop_home.blade.php
@@ -7,9 +7,14 @@
       <p style="font-size: 25px; text-align: left">
         Connecting biologists with microfluidics / engineering, information, and networking tools
       </p>
-      <img src="/images/welcome_bg.jpg" style="margin: auto; max-width: 100%">
+      <img src="/images/welcome_bg.jpg" style="margin: auto; width: 100%" usemap="#image-map">
+      <map name="image-map">
+          <area alt="" title="Information" href="{{ route('news') }}" coords="142,281,58" shape="circle">
+          <area alt="" title="Networking" href="/network" coords="314,254,97" shape="circle">
+          <area alt="" title="Equipment" href="{{ route('product.all') }}" coords="544,224,123" shape="circle">
+      </map>
     </div>
-    <div class="col-xs-12 col-md-10 col-lg-8 text-center" style="position: relative; margin: auto">
+    <div class="col-xs-12 col-md-10 col-lg-8 text-center" style="position: relative; margin: auto; margin-top: 3rem">
       <a href="/news_covid" class="btn btn-danger">COVID-19</a>
       <a href="{{ route('news') }}" class="btn btn-success">Information</a>
       <a href="{{ route('product.all') }}" class="btn btn-info">Equipment</a>
@@ -25,5 +30,8 @@
 @endpush
 
 @push('scripts')
-
+  <script src="/js/imageMapResizer.min.js"></script>
+  <script>
+    $('map').imageMapResize();
+  </script>
 @endpush
