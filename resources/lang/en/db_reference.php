d7711569f934590c52b8b6c9b8d60a13965b47d7
guangxu0924 <guangxu924@yandex.com>
2021-01-06 03:48:32 +1000

    free posting

---
+++ resources/lang/en/db_reference.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'admin'                 => [
+        'title'                   => 'Reference',
+
+        'create_success'          => 'Create new item success!',
+        'edit_success'            => 'Edit item success!',
+        'export_success'          => 'Export success!',
+        'delete'                  => 'Delete',
+        'edit'                    => 'Edit',
+        'add_new'                 => 'Add new',
+        'add_new_individual'      => 'Add new individual',
+        'add_new_company'         => 'Add new company',
+        'add_new_individual_des'  => 'Create a new individual',
+        'add_new_company_des'     => 'Create a new company',
+        'import'                  => 'Import',
+        'export'                  => 'Export',
+        'save_db'                 => 'Save in DB',
+        'refresh'                 => 'Refresh',
+        'result_item'             => 'Showing <b>:item_from</b> to <b>:item_to</b> of <b>:item_total</b> items</b>',
+        'sort'                    => 'Sort',
+        'select_category'         => 'Select category',
+
+        'sort_order'              => [
+            'id_asc'     => 'ID asc',
+            'id_desc'    => 'ID desc',
+            'name_asc'   => 'Name asc',
+            'name_desc'  => 'Name desc',
+            'sku_asc'    => 'SKU a-z',
+            'sku_desc'   => 'SKU z-a',
+            'price_asc'  => 'Price a-z',
+            'price_desc' => 'Price z-a',
+            'view_desc' => 'View desc',
+            'view_asc' => 'View asc',
+            'sold_desc' => 'Sold desc',
+            'sold_asc' => 'Sold asc',
+        ],
+        'search'                  => 'Search',
+        'search_place'            => 'Search Name, Company or ID',
+        'cant_remove_child'        => 'Please remove list products from Builds or Groups before delete them',
+    ]
+];
