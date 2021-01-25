50dd1a0abfcb300382881778de2a6aa040b99a13
Colin Tooker <75869992+colintooker@users.noreply.github.com>
2021-01-25 17:33:37 -0500

    Update shop_home.blade.php
    Container adjustment for pre-web admin content implementation

--- resources/views/templates/default/shop_home.blade.php
+++ resources/views/templates/default/shop_home.blade.php
@@ -2,7 +2,7 @@
 
 @section('main')
   <div class="row welcome">
-    <div class="col-xs-12 col-md-10 col-lg-8" style="margin: auto;font-family: Karla;">
+    <div class="col-xs-10 col-md-8 col-lg-9" style="margin: auto;font-family: Karla;">
       <h1 style="font-family: Karla;font-size: 30px; font-weight: 500">{{ $title }}</h1>
       {!! sc_html_render($page->content) !!}
       <!-- <p style="font-size: 20px; text-align: left">
