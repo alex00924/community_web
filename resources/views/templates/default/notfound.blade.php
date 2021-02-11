09c175f0deab90385f94e302d0a2ac5eff07143c
Colin Tooker <75869992+colintooker@users.noreply.github.com>
2021-02-11 10:10:39 -0500

    404 Error Page Re-work
    Additional implementation needed for directories, generally, and html files not residing on root directory.

--- resources/views/templates/default/notfound.blade.php
+++ resources/views/templates/default/notfound.blade.php
@@ -7,7 +7,10 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12 text-danger text-center">
-                   <h1>{{ $msg??'' }}</h1>
+                  <h1>Oops! 404 Error </h1>
+					<h3>The page you're looking for does not exist</h3>
+					<br>
+					<a class="button-404-error" href="https://fluidsforlife.com/"><i class="icon-home"></i> Return To Homepage</a>
                 </div>
                 </div>
             </div>
