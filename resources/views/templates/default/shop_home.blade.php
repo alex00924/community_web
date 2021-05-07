e3b176e8e1d8974dc98ab53a3f39430b9cdbed3d
Colin Tooker <75869992+colintooker@users.noreply.github.com>
2021-05-07 10:54:28 -0400

    Jira Ticket #FW-192
    Improvements to the Mobile CMS Pages

--- resources/views/templates/default/shop_home.blade.php
+++ resources/views/templates/default/shop_home.blade.php
@@ -2,7 +2,7 @@
 
 @section('main')
   <div class="row welcome">
-    <div class="col-xs-10 col-md-8 col-lg-9" style="margin: auto;font-family: Karla;">
+    <div class="col-xs-10 home col-md-8 col-lg-9" style="margin: auto;font-family: Karla;">
       <h1 style="font-family: Karla;font-size: 30px; font-weight: 500">{{ $title }}</h1>
       {!! sc_html_render($page->content) !!}
     
